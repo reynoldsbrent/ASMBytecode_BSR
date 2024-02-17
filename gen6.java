66422b538e8f6e303343f93e16ba9b633ebfe9bf
reynoldsbrent <brentsreynolds5@gmail.com>
2024-02-17 12:07:39 -0500

    Fixed Scanner error

--- gen6.java
+++ gen6.java
@@ -20,30 +20,40 @@ public class gen6 {
         MethodVisitor main = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
         main.visitCode();
 
+        // Initialize scanner and accumulator variables
+        main.visitTypeInsn(Opcodes.NEW, "java/util/Scanner");
+        main.visitInsn(Opcodes.DUP);
+        main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "in", "Ljava/io/InputStream;");
+        main.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/Scanner", "<init>", "(Ljava/io/InputStream;)V", false);
+        main.visitVarInsn(Opcodes.ASTORE, 1); // Store Scanner object in local variable 1
+
         // Read integer input
+        main.visitVarInsn(Opcodes.ALOAD, 1); // Load Scanner object
         main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
         main.visitLdcInsn("Enter an integer: ");
         main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
-        main.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Scanner", "nextInt", "()I", false);
-        main.visitVarInsn(Opcodes.ISTORE, 1);
+        main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Scanner", "nextInt", "()I", false);
+        main.visitVarInsn(Opcodes.ISTORE, 3);
 
         // Read long input
+        main.visitVarInsn(Opcodes.ALOAD, 1); // Load Scanner object
         main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
         main.visitLdcInsn("Enter a long: ");
         main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
-        main.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Scanner", "nextLong", "()J", false);
+        main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Scanner", "nextLong", "()J", false);
         main.visitVarInsn(Opcodes.LSTORE, 2);
 
         // Read double input
+        main.visitVarInsn(Opcodes.ALOAD, 1); // Load Scanner object
         main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
         main.visitLdcInsn("Enter a double: ");
         main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
-        main.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Scanner", "nextDouble", "()D", false);
+        main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Scanner", "nextDouble", "()D", false);
         main.visitVarInsn(Opcodes.DSTORE, 4);
 
         // Print input values
         main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        main.visitVarInsn(Opcodes.ILOAD, 1);
+        main.visitVarInsn(Opcodes.ILOAD, 3);
         main.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
 
         main.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
